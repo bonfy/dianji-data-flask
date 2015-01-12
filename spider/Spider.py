6ff0dc34285aee0c2600e3552abfe25500efb367
bonfy <foreverbonfy@163.com>
2015-01-12 21:19:06 +0800

    add imp3 spider

--- spider/Spider.py
+++ spider/Spider.py
@@ -10,6 +10,7 @@ from project.models import Scrap
 from config import SPIDER_DATE, SPIDER_PAGE, SPIDER_HEADER
 from project import db
 from bs4 import BeautifulSoup
+import re
 
 
 class Spider(object):
@@ -26,6 +27,8 @@ class Spider(object):
     def run(self):
         if self.website_id == 1:
             self.spider_erjinet()
+        elif self.website_id == 2:
+            self.spider_imp3()
         elif self.website_id == 3:
             self.spider_fengniao()
 
@@ -40,7 +43,7 @@ class Spider(object):
 
     def spider_erjinet(self):
         website = 'http://www.erji.net/'
-        for page in range(1, self.page):
+        for page in xrange(1, self.page):
             url = 'http://www.erji.net/thread.php?fid=8&search=&page=%d' % page
             print 'trace url:', url
             r = requests.get(url, headers=self.headers)
@@ -97,7 +100,7 @@ class Spider(object):
 
     def spider_fengniao(self):
         website = 'http://bbs.fengniao.com'
-        for page in range(1, self.page):
+        for page in xrange(1, self.page):
             # http://bbs.fengniao.com/forum/forumdisplay.php?f=81&type=list&page=1&sort=threadid&order=desc#new
             url = 'http://bbs.fengniao.com/forum/forumdisplay.php?f=81&type=list&page=%d&sort=threadid&order=desc#new' % page
             # print url
@@ -157,3 +160,83 @@ class Spider(object):
                 db.session.commit()
             except Exception as e:
                 print 'insert Table error,Error is',e
+
+
+    def spider_imp3(self):
+        for page in xrange(1, self.page):
+            url = 'http://bbs.imp3.net/forum-63-%d.html' % page
+            r = requests.get(url, headers=self.headers)
+            soup = BeautifulSoup(r.text,"html.parser")
+            trGroups = soup.find_all("tbody",id=re.compile("normalthread_"))
+
+            for trMine in trGroups:
+                scrap = Scrap()
+
+                scrap.title = trMine.find('a',class_="s xst").get_text()
+                scrap.detailUrl = trMine.find('a',class_="s xst").get('href')
+
+                emList = trMine.find_all('em')
+                scrap.keyWord = emList[0].get_text()[1:-1]
+                scrap.sale = emList[1].get_text()[1:-1]
+                scrap.postDate = emList[2].get_text()
+                scrap.viewCount = int(emList[3].get_text().strip())
+                scrap.lastReplyDate = emList[4].get_text()
+                scrap.replyCount = int(trMine.find('a',class_="xi2").get_text().strip())
+
+                userList = trMine.find_all('cite')
+                scrap.author = userList[0].find('a').get_text()
+                scrap.lastReplyName = userList[0].find('a').get_text()
+                scrap.website_id = self.website_id
+
+                print scrap.detailUrl
+                print scrap.postDate
+
+                if scrap.postDate > self.default_date:
+                    print 'date is right'
+                    if self.isDetailUrl(scrap.detailUrl):
+                        print 'trace'
+                        rContent = requests.get(scrap.detailUrl, headers=self.headers)
+                        rSoup = BeautifulSoup(rContent.text, "html.parser")
+                        rtable = rSoup.find("table",class_="cgtl mbm")
+
+
+                        if rtable:
+                            scrap.pdtSale = rtable.caption.get_text().strip()
+
+                            print scrap.pdtSale
+
+                            if scrap.pdtSale != u'其它话题':
+                                if rtable.findAll("tr"):
+                                    trs = rtable.findAll("tr")
+                                    scrap.pdtName = trs[0].td.get_text().strip()
+                                    scrap.pdtNum = trs[1].td.get_text().strip()
+                                    scrap.pdtNew = trs[2].td.get_text().strip()
+                                    scrap.pdtPrice = trs[3].td.get_text().strip()
+                                    scrap.pdtLocation = trs[4].td.get_text().strip()
+                                    scrap.pdtSaleWay = trs[5].td.get_text().strip()
+                                    scrap.pdtTransFee = trs[6].td.get_text().strip()
+                                    scrap.pdtTel = trs[7].td.get_text().strip()
+                                    scrap.pdtValidTime = trs[8].td.get_text().strip()
+                                    if scrap.pdtSale == u'求购':
+                                        scrap.pdtDes = ''
+                                        scrap.pdtUrl = ''
+                                    else:
+                                        scrap.pdtDes = trs[9].td.get_text().strip()
+                                        scrap.pdtUrl = trs[10].td.get_text().strip()
+                        if rSoup.find('td',id = re.compile("postmessage")):
+                            scrap.content = rSoup.find('td',id = re.compile("postmessage")).get_text().strip()
+                        else:
+                            scrap.content = 'TimeOut or some error'
+                        try:
+                            print scrap
+                            db.session.add(scrap)
+                        except Exception as e:
+                            print e
+                            print str(scrap.content)
+                            print 'error in title:', scrap.title
+                    else:
+                        'date is too old'
+            try:
+                db.session.commit()
+            except Exception as e:
+                print 'insert Table error,Error is', e
